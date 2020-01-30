adddbd24c1a71a55925833e274b516aa34bf6ecc
Roman <41066029+iamr0man@users.noreply.github.com>
2020-01-30 23:31:58 +0200

    fix problem with folder

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,22 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults'
 
 import SearchBar from '../components/SearchBar'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([])
-    
-    const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                term,
-                location: 'san jose',
-                limit: 50,
-            }
+    const [searchApi, results] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price
         })
-        setResults(response.data.business)
     }
 
     return (
@@ -27,6 +23,9 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi()}
             />
             <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice('$')}/>
+            <ResultsList results={filterResultsByPrice('$$')}/>
+            <ResultsList results={filterResultsByPrice('$$$')}/>
         </View>
     )
 }
